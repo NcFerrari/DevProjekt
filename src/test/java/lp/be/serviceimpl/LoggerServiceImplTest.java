7f6a92f7e2bb4de079040f4557a6b9ff528be636
Lubor Pešek <lubor.pesek@qinshift.com>
2024-02-26 14:53:52 +0100

    LoggerService unit test

---
+++ src/test/java/lp/be/serviceimpl/LoggerServiceImplTest.java
@@ -0,0 +1,27 @@
+package lp.be.serviceimpl;
+
+import lp.be.service.LoggerService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class LoggerServiceImplTest {
+
+    @Test
+    void getInstance() {
+        LoggerService loggerService = LoggerServiceImpl.getInstance(LoggerServiceImplTest.class);
+        assertNotNull(loggerService);
+    }
+
+    @Test
+    void getInstanceOverloaded() {
+        LoggerService loggerService = LoggerServiceImpl.getInstance(LoggerServiceImplTest.class, true);
+        assertNotNull(loggerService);
+    }
+
+    @Test
+    void getLog() {
+        LoggerService loggerService = LoggerServiceImpl.getInstance(LoggerServiceImplTest.class);
+        assertNotNull(loggerService.getLog());
+    }
+}
